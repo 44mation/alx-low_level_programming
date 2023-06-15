1a05f0470ded248e880ce9ccfc21e82514826674
Hossni <pr.hossni@gmail.com>
2023-06-15 15:57:55 -0700

    compile

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - to enter
+ * Description : a program that prints a phrase with put
+ * Return: a 0 means SUCCES
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

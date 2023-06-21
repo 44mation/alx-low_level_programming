37d2905c6ac393982f23d1323c9b81c59dd19c86
Hossni <pr.hossni@gmail.com>
2023-06-20 18:11:59 -0700

    the absolut of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main code
+ *
+ * @n: the input
+ * Description: return the absolute of a number
+ *
+ * Return: return the abs
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n
+	return (n);
+}

dad822f3385e79c11445efb4ed8c3393d13a9723
Hossni <pr.hossni@gmail.com>
2023-06-26 16:46:40 -0700

    n9ez 1 string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,8 +6,13 @@
 
 void puts2(char *str)
 {
-        while (*str != '\0')
-                _putchar(*(str + 2));
+	int i;
 
-        _putchar('\n');
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }

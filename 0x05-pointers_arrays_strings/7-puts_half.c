068976be9ecb10d6647137aade1f5393281a90a4
Hossni <pr.hossni@gmail.com>
2023-06-26 17:15:58 -0700

    print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - fct prints half of a sting
+ * @str: the string
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	i = strlen(str) / 2;
+	if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	else
+	{
+		i = (strlen(str) - 1) / 2;
+		_putchar(str[I];
+		i++;
+	}
+
+	_putchar('\n');
+}

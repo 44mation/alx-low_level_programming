7d09cba595d1fceddb116b44b68cbcc375f8b7fd
Hossni <pr.hossni@gmail.com>
2023-07-05 16:51:32 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,8 @@
  * _pow_recursion - calculate the powwer of a number
  * @x: the first input
  * @y: the seconde input
- * Return: the result of a power*/
+ * Return: the result of a power
+*/
 
 int _pow_recursion(int x, int y)
 {

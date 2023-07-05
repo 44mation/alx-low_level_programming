5f2e30da81339cb128dc0d209e49ab6afce7fec1
Hossni <pr.hossni@gmail.com>
2023-07-05 16:46:22 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,5 +4,16 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x , y - 1));
+	}
 }

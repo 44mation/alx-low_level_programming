26d9793c5899c2c33f052946b1fa618cb9e17288
Hossni <pr.hossni@gmail.com>
2023-07-05 17:27:51 -0700

    the square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
-/***/
+/**
+ * _sqrt_recursion - calcul the root square
+ * @n: the input
+ * return: the answer
+*/
 
+int square(int n, int x);
 int _sqrt_recursion(int n)
 {
-	return (square(n, 1);
+	return (square(n, 1));
 }
 
-/***/
+/**
+ * square - calcul the square
+ * @n: first input
+ * @x: seconde input
+ * return: the answer
+*/
 
 int square(int n, int x)
 {

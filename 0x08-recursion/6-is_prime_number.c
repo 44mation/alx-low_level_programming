549f3ee607c0ffc148e378b8e59f9efa03fe8212
Hossni <pr.hossni@gmail.com>
2023-07-05 17:47:26 -0700

    the square root

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks a number if prime or not
+ * @n: input 1
+ * @x: input 2
+ * return: true 1 or false 0
+*/
+
+int prime(int n, int x);
+int is_prime_number(int n)
+{
+	return(prime(n, 2));
+}
+
+/**
+ * prime - the function that checks
+ * @n: first input
+ * @x: seconde input
+ * return: the number check
+*/
+
+int prime(int n, int x)
+{
+	if (x >= n && n > 1)
+		return (1);
+	else if (n % x == 0 || n <= 1)
+		return (0);
+	else
+		return (prime(n, x + 1));
+}

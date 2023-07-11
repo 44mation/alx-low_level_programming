91f6d8e960d17affe600b92169072bf75122241b
Hossni <pr.hossni@gmail.com>
2023-07-12 00:59:39 +0100

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,16 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (; str[size] != '\0'; size++)
-
+	;
 		a = malloc(size * sizeof(*str) + 1);
 
 	if (a == 0)
+	{
 		return (NULL);
+	}
 	else
 	{
-		for(; i < size; i++)
+		for (; i < size; i++)
 			a[i] = str[i];
 	}
 	return (a);

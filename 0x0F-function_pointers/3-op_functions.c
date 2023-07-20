a0b9e726047bd14dcca829ca05718b3c3ec18d23
Hossni <pr.hossni@gmail.com>
2023-07-20 01:24:56 +0100

    operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition opperation
+ * @a: the first input
+ * @b: the seconde input
+ * Return: return sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - subs opperation
+ * @a: the first input
+ * @b: the seconde input
+ * Return: return substruction
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication opperation
+ * @a: the first input
+ * @b: the seconde input
+ * Return: return mul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - division opperation
+ * @a: the first input
+ * @b: the seconde input
+ * Return: return div
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo opperation
+ * @a: the first input
+ * @b: the seconde input
+ * Return: return mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

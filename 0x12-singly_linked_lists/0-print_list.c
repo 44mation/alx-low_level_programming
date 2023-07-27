33143b99409a2128ea4b79ffd9683722215357d9
Hossni <pr.hossni@gmail.com>
2023-07-28 00:58:08 +0100

    update task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlent: calculate the lenght of the string
+ * @s: the string to calulate
+ * Return: the lenght
+*/
+
+int _strlent(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * print_list -prints linked lists
  * @h: pointer to head
  * Return: size of the list
@@ -12,7 +29,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d]v%s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", _strlent(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		i++;
 	}

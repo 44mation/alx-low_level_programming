61b1828275bcc840056a131fc38b2262a6e1d8be
Hossni <pr.hossni@gmail.com>
2023-07-28 01:11:13 +0100

    lenght of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - calculate length of list
+ * @h: the head
+ * Return: the length
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

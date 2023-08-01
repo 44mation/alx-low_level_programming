fd0ab95577f2150ba4288253c234f5d54736089f
Hossni <pr.hossni@gmail.com>
2023-08-01 20:00:25 +0100

    length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
 */
 typedef struct listint_s
 {
@@ -21,5 +21,5 @@ typedef struct listint_s
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
-
+size_t listint_len(const listint_t *h);
 #endif

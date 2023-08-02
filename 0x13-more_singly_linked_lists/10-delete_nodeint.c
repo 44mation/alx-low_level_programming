2a1986bfd1248f5b50dda9721e2307e368a6f490
Hossni <pr.hossni@gmail.com>
2023-08-02 10:39:04 +0100

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - insert a node
+ * @head: the list starting point
+ * @index: the index to insert to
+ * Return: the node
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !new_node)
+		return (-1);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prec_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+	return (-1);
+}
